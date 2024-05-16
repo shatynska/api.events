5b94f6184185b066f7a087fe8cd927251a4db868
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-16 22:34:39 +0300

    feat(throttler): set up in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { ParticipantsModule } from './participants/participants.module';
 
 @Module({
-  imports: [ParticipantsModule],
+  imports: [
+    ParticipantsModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
