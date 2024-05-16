4e7561dedcb626c4d3eb550b47ca05aabbf86fd8
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-16 20:24:04 +0300

    refactor(participance): inject repository token instead of direct link to prisma repository

--- src/participants/application/register/register.service.ts
+++ src/participants/application/register/register.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaRepository } from '~/participants/infrastructure/persistence/prisma.repository';
+import { Inject, Injectable } from '@nestjs/common';
 import { RegisterParticipantDto } from '../dto/register-participant.dto';
+import { REPOSITORY_TOKEN, Repository } from '../repository';
 
 @Injectable()
 export class RegisterService {
-  constructor(private repository: PrismaRepository) {}
+  constructor(
+    @Inject(REPOSITORY_TOKEN) private readonly repository: Repository,
+  ) {}
 
   async register(registerParticipantDto: RegisterParticipantDto) {
     // TODO Add check whether the user is already registered for this event
