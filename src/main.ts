ce494bd483372624bf36f6ae6bac3b2e03df3936
Helen Shatynska <helen.shatynska@gmail.com>
2024-05-16 10:12:41 +0300

    feat(openapi): set up openapi document in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { patchNestjsSwagger } from '@anatine/zod-nestjs';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+
+  patchNestjsSwagger();
+  const config = new DocumentBuilder()
+    .setTitle('Events Registration')
+    .setDescription('API for events registration')
+    .setVersion('0.0.1')
+    .addTag('events')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
